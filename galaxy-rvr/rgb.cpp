3da8f62b845a114a1e96d6585af22c46a1fead44
Cavon <cavonxx@gmail.com>
2023-07-03 16:02:57 +0800

    add Arduino UNO R4 compatibility

--- galaxy-rvr/rgb.cpp
+++ galaxy-rvr/rgb.cpp
@@ -1,11 +1,16 @@
 #include "rgb.h"
+
+#if defined(ARDUINO_AVR_UNO)
 #include <SoftPWM.h>
+#endif
 
 void rgbBegin() {
+  #if defined(ARDUINO_AVR_UNO)
   for (uint8_t i = 0; i < 3; i++) {
     SoftPWMSet(RGB_PINS[i], 0);
     SoftPWMSetFadeTime(RGB_PINS[i], 100, 100);
   }
+  #endif
 }
 
 void rgbWrite(uint32_t color) {
@@ -27,9 +32,15 @@ void rgbWrite(uint8_t r, uint8_t g, uint8_t b) {
     b = 255 - b;
   #endif
   // set volatge 
+  #if defined(ARDUINO_AVR_UNO)
   SoftPWMSet(RGB_PINS[0], r);
   SoftPWMSet(RGB_PINS[1], g);
   SoftPWMSet(RGB_PINS[2], b);
+  #elif defined(ARDUINO_MINIMA)
+  analogWrite(RGB_PINS[0], r);
+  analogWrite(RGB_PINS[1], g);
+  analogWrite(RGB_PINS[2], b);
+  #endif
 }
 
 void rgbOff() {
